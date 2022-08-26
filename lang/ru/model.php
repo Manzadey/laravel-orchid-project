86a3b764f9f703392ada67190d56e8a3ab666fd0
Andrey Manzadey <andrey.manzadey@gmail.com>
2022-08-26 12:52:23 +0300

    Update model.php

--- lang/ru/model.php
+++ lang/ru/model.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'users'      => 'Пользователи',
-    'roles'      => 'Роли',
+    'users' => 'Пользователи',
+    'roles' => 'Роли',
     'activities' => 'Логи изменения данных',
-    'http-logs'  => 'Логи HTTP',
-    'jobs'       => 'Менеджер очередей',
+    'http-logs' => 'Логи HTTP',
+    'jobs' => 'Менеджер очередей',
 ];
-
